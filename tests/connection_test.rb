6ba5ad250b134fa80c4951dee6e0b09e3ff3b4dc
Terrence Lim <hlim1@email.arizona.edu>
2019-10-09 11:37:24 -0700

    Added comments above for each code to exaplain their functionalities

--- tests/connection_test.rb
+++ tests/connection_test.rb
@@ -1,9 +1,13 @@
 require 'rubySQL'
 
-def my_connection()
+def main
+  # Create a RubySQL object
   rbsql = RubySQL.new
+  # Connect to specified SQLite3 database.
+  # Create if DB not exist or open the existing one.
   rbsql.connect("DB_test.db")
+  # Check the current version of SQLite3
   rbsql.version
 end
 
-my_connection()
+main
