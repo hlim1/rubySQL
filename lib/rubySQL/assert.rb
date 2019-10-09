da6fe2f4d9d682a776ec7220d9db2b9ddfa3e9c9
Terrence Lim <hlim1@email.arizona.edu>
2019-10-09 10:58:59 -0700

    Adding Assert file

---
+++ lib/rubySQL/assert.rb
@@ -0,0 +1,9 @@
+class RubySQL::Assert
+  def self.check_db_name(db_name)
+    if not db_name.is_a?(String)
+      puts "Invalid db_name type. db_name must given in String."
+      printf "User input <%s> that is type <%s>\n", db_name, db_name.class
+      exit
+    end
+  end
+end
