66cec368bb67a0490cc713d8311aeacd91faf7cf
Terrence Lim <hlim1@email.arizona.edu>
2019-10-11 04:09:11 -0700

    Added more test cases to table creation test

--- tests/create_table_test.rb
+++ tests/create_table_test.rb
@@ -5,12 +5,19 @@ def main
   rbsql = RubySQL.new
   # Connect to specified SQLite3 database.
   # Create if DB not exist or open the existing one.
-  rbsql.connect("DB_test.db")
+  rbsql.connect("DB_test")
   # Check the current version of SQLite3
   rbsql.version
 
   # rbsql.create_table("COMPANY").with([["__type__", "__column_name__", "__nullable__"],...,]).primary_key(__column_name__)
-  rbsql.create_table("COMPANY").with(["ID" => ["INT", "NO"], "NAME" => ["TEXT", "NO"], "AGE" => ["INT", "NO"], "ADDRESS" => ["TEXT", "YES"], "SALARY" => ["REAL", "YES"]]).primary("ID")
+  rbsql.create_table("COMPANY_A").with(["ID" => ["INT", "NO"], "NAME" => ["TEXT", "NO"], "AGE" => ["INT", "NO"], "ADDRESS" => ["TEXT", "YES"], "SALARY" => ["REAL", "YES"]]).primary("ID")
+  rbsql.create_table("COMPANY_B").with(["ID" => ["INT", "NO"], "NAME" => ["TEXT", "NO"], "AGE" => ["INT", "NO"], "ADDRESS" => ["TEXT", "YES"], "SALARY" => ["REAL", "YES"]]).primary("ID")
+
+  # Print table schema
+  rbsql.schema_of("COMPANY_A")
+  rbsql.schema_of("COMPANY_B")
+
+  rbsql.list_tables
 
   # Close DB
   rbsql.close
