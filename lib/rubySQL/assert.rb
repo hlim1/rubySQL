a1d00305631d8ba5477c80ffe31df4fb096bd355
Terrence Lim <hlim1@email.arizona.edu>
2019-10-24 23:55:47 -0700

    Added a default assert for printing error message and added documentation comments

--- lib/rubySQL/assert.rb
+++ lib/rubySQL/assert.rb
@@ -11,9 +11,9 @@ class RubySQL::Assert
   ]
 
   # Checks validity of database name that was input from the user.
-  # Database name must be a string type. Error and exist otherwise.
+  # Database name must be a string type. Error and exit otherwise.
   # Params:
-  # - db_name (str): Database name
+  # - db_name (str): Database name.
   # Returns:
   # - None
   def self.check_db_name(db_name)
@@ -24,6 +24,13 @@ class RubySQL::Assert
     end
   end
 
+  # Check validity of table name that was input from the user.
+  # Table name must be given in a string type. Error and exit otherwise.
+  # Params:
+  # - table_name (str): Table name.
+  # - dbh (DB obj): Database handler.
+  # Returns:
+  # - None.
   def self.check_table_name(table_name, dbh)
     if not table_name.is_a?(String)
       puts "Error: Invalid table_name type. Table name type must be String."
@@ -38,7 +45,7 @@ class RubySQL::Assert
   # this assert method will given an error and exit after closing the database safely.
   # Params:
   # - status (bool): true if table exists, false, if table does not exists.
-  # - table_name (str): Table name
+  # - table_name (str): Table name.
   # Returns:
   # - None
   def self.table_already_exist(status, table_name, dbh)
@@ -58,6 +65,15 @@ class RubySQL::Assert
     #}
   end
 
+  # Assert for user trying to access none existing table in the database.
+  # Check and status will be done in the method that calls this assert method.
+  # This method simply prints error message and exit on check failure.
+  # Params:
+  # - status (int): 1 (or true) for success or 0 (or false) for fail.
+  # - table_name (str): Table name.
+  # - dbh (DB obj): Database handlerfor closing the DB.
+  # Returns:
+  # - None.
   def self.table_not_exist(status, table_name, dbh)
     if status == false or status == nil
       printf "Error: Table #{table_name} does not exist in the database.\n"
@@ -70,7 +86,7 @@ class RubySQL::Assert
   # in the @sqlite3_types array, give an error and exit after safely closing
   # the database.
   # Params:
-  # - type (str): User input type
+  # - type (str): User input type.
   # Returns:
   # - None
   def self.check_type(type)
@@ -81,6 +97,14 @@ class RubySQL::Assert
     end
   end
 
+  # Compare the user input value class with the required value class.
+  # If fail, prints error and exit.
+  # Params:
+  # - input_class (class): User input value class.
+  # - compare_class (class): Require value class.
+  # - dbh (DB obj): Database handlerfor closing the DB.
+  # Returns:
+  # - None.
   def self.check_class(input_class, compare_class, dbh)
     if input_class != compare_class
       puts "Error: Invalid value class (#{input_clas})."
@@ -92,5 +116,21 @@ class RubySQL::Assert
 
   def self.column_not_exist(status, dbh)
 
+  end
+
+  # Check for the passed status and print out error message
+  # and  terminate the program or do nothing.
+  # Params:
+  # - status (int): 1 (or true) for success or 0 (or false) for fail.
+  # - msg (str): Error message to be printed out in case of fail.
+  # - dbh (DB obj): Database handlerfor closing the DB.
+  # Returns:
+  # - None.
+  def self.error_message(status, msg, dbh)
+    if status == 0 or status == false
+      puts msg
+      dbh.close if dbh
+      exit
+    end
   end
 end
