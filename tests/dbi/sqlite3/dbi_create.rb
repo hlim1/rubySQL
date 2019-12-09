75f5487ff233d0080e6469e0341865fa712e4b96
Terrence Lim <hlim1@email.arizona.edu>
2019-12-08 21:42:10 -0700

    Modified dbi create table test file

--- tests/dbi/sqlite3/dbi_create.rb
+++ tests/dbi/sqlite3/dbi_create.rb
@@ -1,32 +1,28 @@
 require "dbi"
-require "benchmark"
 
-puts Benchmark.measure {
-  begin
-     # connect to the MySQL server
-      dbh = DBI.connect("DBI:SQLite3:myTestDB", "temp", "tempPwd")
-     # get server version string and display it
-     version = dbh.select_one("SELECT SQLITE_VERSION()")
-     puts version[0]
-     # create table
-     dbh.do("CREATE TABLE IF NOT EXISTS EMPLOYEES (
-     ID INT PRIMARY KEY NOT NULL,
-     FIRST_NAME  TEXT NOT NULL,
-     LAST_NAME  TEXT,
-     AGE INT,  
-     SEX TEXT,
-     INCOME REAL )" )
-     sth = dbh.execute("select * from sqlite_master where type='table';")
-     while row = sth.fetch do
-       puts row[1], row[4]
-     end
-     sth.finish
-  rescue DBI::DatabaseError => e
-     puts "An error occurred"
-     puts "Error code:    #{e.err}"
-     puts "Error message: #{e.errstr}"
-  ensure
-     # disconnect from server
-     dbh.disconnect if dbh
-  end
-}
+begin
+   # connect to the MySQL server
+   dbh = DBI.connect("DBI:SQLite3:dbiTestDB", "temp", "tempPwd")
+
+   # create two tables
+   dbh.do("CREATE TABLE IF NOT EXISTS COMPANY_A (
+   ID INT PRIMARY KEY NOT NULL,
+   NAME TEXT NOT NULL,
+   AGE INT NOT NULL,  
+   ADDRESS TEXT,
+   SALARY REAL )" )
+
+   dbh.do("CREATE TABLE IF NOT EXISTS COMPANY_B (
+   ID INT PRIMARY KEY NOT NULL,
+   NAME TEXT NOT NULL,
+   AGE INT NOT NULL,  
+   ADDRESS TEXT,
+   SALARY REAL )" )
+rescue DBI::DatabaseError => e
+   puts "An error occurred"
+   puts "Error code:    #{e.err}"
+   puts "Error message: #{e.errstr}"
+ensure
+   # disconnect from server
+   dbh.disconnect if dbh
+end
