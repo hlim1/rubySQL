02add1098fa3e8644aa2da4364a8b5e87e7a7040
Terrence Lim <hlim1@email.arizona.edu>
2019-12-09 21:20:38 -0700

    Modified test file for select

--- tests/select.rb
+++ tests/select.rb
@@ -7,13 +7,18 @@ rbsql = RubySQL.new
 # Create if DB not exist or open the existing one.
 rbsql.connect("DB_test")
 
-rows_b = rbsql.select_all("COMPANY_A")
 puts "ROWS in Table COMPANY_A using select_all method:"
+rows_a = rbsql.select_all("COMPANY_A")
+puts rows_a
+
+puts "COLUMNS in Table COMPANY_A using select_from method:"
+rows_b = rbsql.select_from("COMPANY_A", "col").on_columns(["ID", "NAME", "AGE", "SALARY"])
 puts rows_b
 
-rows_a = rbsql.select_from("COMPANY_A", "col").on_columns(["ID", "NAME", "AGE"])
-puts "COLUMNS in Table COMPANY_A using select_from method:"
-puts rows_a
+puts "SELECT with condition in Table COMPANY_A using select_from method:"
+rows_b = rbsql.select_from("COMPANY_A", "row").where(">=", "SALARY", 650000).on_columns(["ID", "NAME", "AGE", "SALARY"])
+puts rows_b
+
 
 # rbsql.select_from("COMPANY_A").with_condition("No Condition").on_columns(["*"])
 
